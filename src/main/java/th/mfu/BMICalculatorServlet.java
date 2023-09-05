479e4c8d22bbad9275f158e6d938beb15b5d5f73
moepyaePK <6531503158@lamduan.mfu.ac.th>
2023-09-05 15:49:08 +0700

    perfect

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,20 +9,46 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
-public class BMICalculatorServlet extends HttpServlet{
+@WebServlet(urlPatterns = "/calbmi")
+public class BMICalculatorServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO: get parameter from request: "weight" and "height"
+        String weightInstring = request.getParameter("weight");
+        String heightInstring = request.getParameter("height");
+        response.setContentType("text/html");
+        // TODO: calculate bmi
+
+        double weight = Double.parseDouble(weightInstring);
+        double height = Double.parseDouble(heightInstring);
+
+        double result = weight / (height * height);
+        int final_result = (int) Math.round(result);
+
+        // TODO: determine the built from BMI
+        String BMI_result;
+        if (result < 18.5) {
+            BMI_result = "underweight";
+        } else if (18.5 <= result && result < 25) {
+            BMI_result = "normal";
+        } else if (25 <= result && result < 30) {
+            BMI_result = "overweight";
+        } else if (30 <= result && result < 35) {
+            BMI_result = "obese";
+        } else {
+            BMI_result = "extremely obese";
+        }
+
+        // TODO: add bmi and built to the request's attribute
+
+        request.setAttribute("BMI_result", BMI_result);
+        request.setAttribute("final_result", final_result);
         
-        //TODO: calculate bmi
 
-        //TODO: determine the built from BMI
-      
-        //TODO: add bmi and built to the request's attribute
-
-        //TODO: forward to jsp
-           
+        // TODO: forward to jsp
+        request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
     }
-    
+
 }
--- src/test/java/th/mfu/TestBMICalculatorIT.java
+++ src/test/java/th/mfu/TestBMICalculatorIT.java
@@ -36,7 +36,7 @@ public class TestBMICalculatorIT {
     public void testCaclulate1() {
 
         // Make a HTTP GET request to retrieve the last created Parolee.
-        try (Response response = client.target(WEB_URI+"?weight=70&height=1.5").request().get()) {
+        try (Response response = client.target(WEB_URI + "?weight=70&height=1.5").request().get()) {
 
             // Check that the HTTP response code is 200 OK.
             int responseCode = response.getStatus();
@@ -50,12 +50,13 @@ public class TestBMICalculatorIT {
         }
     }
 
-    //TODO: add another test case for normal built such as weight=50 and height=1.5.
+    // TODO: add another test case for normal built such as weight=50 and
+    // height=1.5.
     @Test
     public void testCaclulate2() {
 
         // Make a HTTP GET request to retrieve the last created Parolee.
-        try (Response response = client.target(WEB_URI+"?weight=50&height=1.5").request().get()) {
+        try (Response response = client.target(WEB_URI + "?weight=50&height=1.5").request().get()) {
 
             // Check that the HTTP response code is 200 OK.
             int responseCode = response.getStatus();
@@ -68,5 +69,25 @@ public class TestBMICalculatorIT {
             _logger.info("IT1 test passed");
         }
     }
+
+    @Test
+    public void testCaclulate3() {
+
+        // Make a HTTP GET request to retrieve the last created Parolee.
+        try (Response response = client.target(WEB_URI + "?weight=53&height=1.6").request().get()) {
+
+            // Check that the HTTP response code is 200 OK.
+            int responseCode = response.getStatus();
+            assertEquals(200, responseCode);
+
+            String jsonResponse = response.readEntity(String.class);
+            assertThat(jsonResponse, CoreMatchers.containsString("Result is 21"));
+
+            assertThat(jsonResponse, CoreMatchers.containsString("normal"));
+            _logger.info("IT1 test passed");
+        }
+    }
+
+   
 
 }
